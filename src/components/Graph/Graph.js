98833d59be2dba91606e48a285dc686d0cf9af34
Mohammed-Ibrahim-15 <ibrahim72342@gmail.com>
2022-10-11 15:06:01 +0600

    graph and pie chart added

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { LineChart, Line } from 'recharts';
 
-const Graph = () => {
+const Graph = ({ data }) => {
+    const { total, name } = data
     return (
         <div>
+            <LineChart width={400} height={400} data={data}>
+                <Line type="monotone" dataKey={total} stroke="#8884d8" />
+            </LineChart>
 
         </div>
     );
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer, PieChart, Pie } from 'recharts';
 
 const Statistics = () => {
     const data = useLoaderData().data;
     console.log(data)
     return (
-        <div>
-            <h2>Statistics</h2>
+        <div className='bg-gray-100'>
+            <h2 className='text-2xl font-bold py-6 mb-6'>Statistics of Our Quiz and Questions</h2>
+            <div className='flex justify-center items-center flex-col'>
+                <div>
+                    <ResponsiveContainer width={400} height={400}>
+                        <LineChart width={400} height={400} data={data}>
+                            <Line type="monotone" dataKey={"total"} stroke="#8884d8" />
+                            <XAxis dataKey="name" />
+                            <YAxis />
+                            <Tooltip />
+                        </LineChart>
+                    </ResponsiveContainer>
+                    <h1>Figure: Line Graph</h1>
+                </div>
+                <div>
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            dataKey="total"
+                            isAnimationActive={false}
+                            data={data}
+                            cx="50%"
+                            cy="50%"
+                            outerRadius={80}
+                            fill="#8884d8"
+                            label
+                        />
+                        <Pie dataKey="total" data={data} cx={500} cy={200} innerRadius={40} outerRadius={80} fill="#82ca9d" />
+                        <Tooltip />
+                    </PieChart>
+                    <h1>Figure: Pie Chart</h1>
+                </div>
+            </div>
         </div>
     );
 };
